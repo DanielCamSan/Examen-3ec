058fe6d8c5e54ff6db53e46a04f21e3e377c7888
unknown <ariana.pita@ucb.edu.bo>
2025-10-31 17:13:00 -0400

    feat: add POST endpoint

--- Controllers/SpeakersController.cs
+++ Controllers/SpeakersController.cs
@@ -9,12 +9,22 @@ namespace _3ecexamen.Controllers
     public class SpeakersController : ControllerBase
     {
         //TODO  pista: usa speaker y talk service
+        private readonly ISpeakerService _speaker;
+        private readonly ITalkService _talk;
+
+        public SpeakersController(ISpeakerService speaker, ITalkService talk)
+        {
+            _speaker = speaker;
+            _talk = talk;
+        }
 
         // POST: api/v1/speakers
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] CreateSpeakerDto dto)
         {
             //TODO
+            var id= await _speaker.CreateAsync(dto);
+            return CreatedAtAction(nameof(GetSchedule), new { id }, new {id});
         }
 
         // GET: api/v1/speakers/{id}/schedule
@@ -22,6 +32,9 @@ namespace _3ecexamen.Controllers
         public async Task<IActionResult> GetSchedule(int id)
         {
             //TODO
+            var data= await _speaker.GetScheduleAsync(id);
+            if (data==null) return NotFound($"No encontrado");
+            return Ok(data);
         }
 
         // POST: api/v1/speakers/talks
@@ -29,6 +42,8 @@ namespace _3ecexamen.Controllers
         public async Task<IActionResult> AddTalk([FromBody] CreateTalkDto dto)
         {
             //TODO
+            await _talk.AddTalkAsync(dto);
+            return Ok();
         }
     }
 }

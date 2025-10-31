31922fb030ee58e9350565811975f732672055b7
yessica rado <yessicarado@gmail.com>
2025-10-31 16:49:07 -0400

    feat:modified conferenceservice

--- Services/ConferenceService.cs
+++ Services/ConferenceService.cs
@@ -1,16 +1,24 @@
 ﻿using _3ecexamen.DTOs;
 using _3ecexamen.Entities;
 using _3ecexamen.Repositories;
+using static System.Reflection.Metadata.BlobBuilder;
 
 namespace _3ecexamen.Services
 {
     public class ConferenceService : IConferenceService
     {
         //TODO
+        private readonly IConferenceRepository _confs;
+
+        public ConferenceService(IConferenceRepository confs) => _confs = confs;
 
         public async Task<int> CreateConferenceAsync(CreateConferenceDto dto)
         {
             //TODO
+            var entity = new Conference { Title = dto.Title, City=dto.City, StartDate=dto.StartDate, EndDate=dto.EndDate };
+            await _confs.AddAsync(entity);
+            await _confs.SaveChangesAsync();
+            return entity.Id;
         }
 
         public async Task<ConferenceAgendaDto?> GetAgendaAsync(int id)
@@ -18,7 +26,27 @@ namespace _3ecexamen.Services
             var conf = await _confs.GetAgendaAsync(id);
             if (conf == null) return null;
             //TODO  pista: devuelve usando ConferenceAgendaDto
-           
-        }
+            return new ConferenceAgendaDto
+            {
+                Conference = conf.Title,
+                City = conf.City,
+                Rooms= conf.Talks
+                .Select(r => new RoomScheduleDto
+                {
+                    Room = r.Name,
+                    Talks = r.Talks
+                    .OrderBy(t => t.StartTime)
+                    .Select(t => new TalkDto
+                    {
+                        SpeakerId = t.SpeakerId,
+                        Speaker = t.Speaker.FullName,
+                        RoomId = t.RoomId,
+                        Room = t.Room.Name,
+                        StartTime = t.StartTime,
+                        EndTime = t.EndTime
+                    }).ToList()
+                }).ToList()
+            };
+         }
     }
 }

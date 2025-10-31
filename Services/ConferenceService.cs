29e64e218976c98c595838e162541bc82db5ed9c
Alejandro Castro <alejandro.castro.0520@gmail.com>
2025-10-31 16:36:34 -0400

    feat: ConferenceService create finished

--- Services/ConferenceService.cs
+++ Services/ConferenceService.cs
@@ -1,16 +1,39 @@
 ﻿using _3ecexamen.DTOs;
 using _3ecexamen.Entities;
 using _3ecexamen.Repositories;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace _3ecexamen.Services
 {
     public class ConferenceService : IConferenceService
     {
         //TODO
+        private readonly IConferenceRepository _repo;
+        public ConferenceService(IConferenceRepository repo)
+        {
+            _repo = repo;
+        }
+
 
         public async Task<int> CreateConferenceAsync(CreateConferenceDto dto)
         {
             //TODO
+            Conference conference = new()
+            {
+                Title = dto.Title,
+                City = dto.City,
+                StartDate = dto.StartDate,
+                EndDate = dto.EndDate,
+                Rooms = dto.Rooms.Select((roomDto) => new Room()
+                {
+                    Name=roomDto.Name,
+                }).ToList(),
+            };
+
+            await _repo.AddAsync(conference);
+            await _repo.SaveChangesAsync();
+
+            return conference.Id;
         }
 
         public async Task<ConferenceAgendaDto?> GetAgendaAsync(int id)

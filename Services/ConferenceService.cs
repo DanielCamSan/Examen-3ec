16ad44aa1f96253abc3b601ac145ec32160a4bcc
RashLop <lopezmejairasshid@outlook.com>
2025-10-31 16:58:06 -0400

    Conference Service fixed

--- Services/ConferenceService.cs
+++ Services/ConferenceService.cs
@@ -1,16 +1,30 @@
 ﻿using _3ecexamen.DTOs;
 using _3ecexamen.Entities;
 using _3ecexamen.Repositories;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace _3ecexamen.Services
 {
     public class ConferenceService : IConferenceService
     {
         //TODO
+        private readonly IConferenceRepository _confs;
+        public ConferenceService(IConferenceRepository confs) => _confs = confs; 
 
         public async Task<int> CreateConferenceAsync(CreateConferenceDto dto)
         {
-            //TODO
+            var created = new Conference
+            {
+                Title = dto.Title,
+                City = dto.City,
+                StartDate = dto.StartDate,
+                EndDate = dto.EndDate
+            };
+            await _confs.AddAsync(created);
+            await _confs.SaveChangesAsync();
+            return created.Id;
+            
         }
 
         public async Task<ConferenceAgendaDto?> GetAgendaAsync(int id)
@@ -18,6 +32,27 @@ namespace _3ecexamen.Services
             var conf = await _confs.GetAgendaAsync(id);
             if (conf == null) return null;
             //TODO  pista: devuelve usando ConferenceAgendaDto
+            var rooms = conf.Rooms.Select(r => new RoomScheduleDto
+            {
+                Room = r.Name,
+                Talks = r.Talks.Select(t => new TalkDto
+                {
+                    SpeakerId = t.SpeakerId,
+                    Speaker = t.Speaker.FullName,
+                    RoomId = t.RoomId,
+                    Room = t.Room.Name,
+                    StartTime = t.StartTime,
+                    EndTime = t.EndTime
+                }).ToList()
+            }).ToList();
+            var agenda = new ConferenceAgendaDto
+            {
+                Conference = conf.Title,
+                City = conf.City,
+                Rooms = rooms
+            };
+            return agenda; 
+
            
         }
     }

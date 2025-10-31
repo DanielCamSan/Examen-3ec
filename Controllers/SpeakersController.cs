7053d7ae19a7b450598f1e6361af10377a84dfa3
unknown <ernes777rim@gmail.com>
2025-10-31 17:21:56 -0400

    Feat: Finished TODOs

--- Controllers/SpeakersController.cs
+++ Controllers/SpeakersController.cs
@@ -10,25 +10,38 @@ namespace _3ecexamen.Controllers
     {
         //TODO  pista: usa speaker y talk service
 
+        private readonly ITalkService _talkService;
+        private readonly ISpeakerService _speakerService;
+
+
+        public SpeakersController(ISpeakerService service, ITalkService talkService)
+        {
+            _speakerService = service;
+            _talkService = talkService;
+        }
         // POST: api/v1/speakers
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] CreateSpeakerDto dto)
         {
             //TODO
+            var id = await _speakerService.CreateAsync(dto);
+            return CreatedAtAction(nameof(GetSchedule), new {id =id}, new { id = id });
         }
 
         // GET: api/v1/speakers/{id}/schedule
         [HttpGet("{id:int}/schedule")]
         public async Task<IActionResult> GetSchedule(int id)
         {
-            //TODO
+            var data = await _speakerService.GetScheduleAsync(id);
+            return data is null ? NotFound(new { error = "Speaker not found", status = 404 }) : Ok(data);
         }
 
         // POST: api/v1/speakers/talks
         [HttpPost("talks")]
         public async Task<IActionResult> AddTalk([FromBody] CreateTalkDto dto)
         {
-            //TODO
+            await _talkService.AddTalkAsync(dto);
+            return Ok();
         }
     }
 }
--- Services/ConferenceService.cs
+++ Services/ConferenceService.cs
@@ -6,7 +6,7 @@ namespace _3ecexamen.Services
 {
     public class ConferenceService : IConferenceService
     {
-        //TODO
+        //TODO DONE
         private readonly IConferenceRepository _confs;
 
         public ConferenceService(IConferenceRepository confs)
@@ -34,8 +34,8 @@ namespace _3ecexamen.Services
         {
             var conf = await _confs.GetAgendaAsync(id);
             if (conf == null) return null;
-            //TODO  pista: devuelve usando ConferenceAgendaDto
-            return new ConferenceAgendaDto
+            //TODO  pista: devuelve usando ConferenceAgendaDto DONE VAAAAMOOOOOOOOOOOOOOOOO
+            return new ConferenceAgendaDto 
             {
                 Conference = conf.Title,
                 City = conf.City,

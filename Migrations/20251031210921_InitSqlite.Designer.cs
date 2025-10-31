6a69c834a09f1547fb3560baa7f85a741928b273
fabio-gmoo <fabio.garciameza@ucb.edu.bo>
2025-10-31 17:14:14 -0400

    feat(Migrations): Creating new migrations

--- Migrations/20251031210921_InitSqlite.Designer.cs
+++ Migrations/20251031210921_InitSqlite.Designer.cs
@@ -11,7 +11,7 @@ using _3ecexamen.Data;
 namespace _3ecexamen.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20251031231927_InitSqlite")]
+    [Migration("20251031210921_InitSqlite")]
     partial class InitSqlite
     {
         /// <inheritdoc />

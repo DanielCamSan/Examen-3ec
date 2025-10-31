8e510bc708debbbdab102aa71fe7d53ade252776
Camilo <camilorodriguezandre@gmail.com>
2025-10-31 16:32:28 -0400

    Feat: TODOs implementation of SpeakersControllers

--- Controllers/SpeakersController.cs
+++ Controllers/SpeakersController.cs
@@ -1,6 +1,7 @@
 ﻿using _3ecexamen.DTOs;
 using _3ecexamen.Services;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace _3ecexamen.Controllers
 {
@@ -8,27 +9,44 @@ namespace _3ecexamen.Controllers
     [Route("api/v1/[controller]")]
     public class SpeakersController : ControllerBase
     {
-        //TODO  pista: usa speaker y talk service
+
+        private readonly ISpeakerService _service;
+        private readonly ITalkService _talkService;
+
+        public SpeakersController(ISpeakerService service, ITalkService talkService)
+        {
+            _service = service;
+            _talkService = talkService;
+        }
 
         // POST: api/v1/speakers
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] CreateSpeakerDto dto)
         {
-            //TODO
+
+            var id = await _service.CreateAsync(dto);
+            return CreatedAtAction(nameof(GetSchedule), new { id = id }, new { id = id });
+
         }
 
         // GET: api/v1/speakers/{id}/schedule
         [HttpGet("{id:int}/schedule")]
         public async Task<IActionResult> GetSchedule(int id)
         {
-            //TODO
+
+            var data = await _service.GetScheduleAsync(id);
+            if (data == null) return NotFound();
+            return Ok(data);
+
         }
 
         // POST: api/v1/speakers/talks
         [HttpPost("talks")]
         public async Task<IActionResult> AddTalk([FromBody] CreateTalkDto dto)
         {
-            //TODO
+            await _talkService.AddTalkAsync(dto);
+            return Ok();
+
         }
     }
 }

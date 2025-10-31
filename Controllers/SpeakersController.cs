984018eefed0ed455d0d64b68286cf03de083bfb
KathM00 <kathymontamejia@gmail.com>
2025-10-31 17:26:20 -0400

    Changes

--- Controllers/SpeakersController.cs
+++ Controllers/SpeakersController.cs
@@ -8,27 +8,39 @@ namespace _3ecexamen.Controllers
     [Route("api/v1/[controller]")]
     public class SpeakersController : ControllerBase
     {
-        //TODO  pista: usa speaker y talk service
+        private readonly ISpeakerService _speakers;
+        private readonly ITalkService _talks;
+
+        public SpeakersController(ISpeakerService speakers, ITalkService talks)
+        {
+            _speakers = speakers;
+            _talks = talks;
+        }
 
         // POST: api/v1/speakers
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] CreateSpeakerDto dto)
         {
-            //TODO
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            var id = await _speakers.CreateAsync(dto);
+            return CreatedAtAction(nameof(GetSchedule), new { id }, new { id });
         }
 
         // GET: api/v1/speakers/{id}/schedule
         [HttpGet("{id:int}/schedule")]
         public async Task<IActionResult> GetSchedule(int id)
         {
-            //TODO
+            var data = await _speakers.GetScheduleAsync(id);
+            if (data == null) return NotFound();
+            return Ok(data);
         }
 
         // POST: api/v1/speakers/talks
         [HttpPost("talks")]
         public async Task<IActionResult> AddTalk([FromBody] CreateTalkDto dto)
         {
-            //TODO
+            await _talks.AddTalkAsync(dto);
+            return Ok();
         }
     }
 }

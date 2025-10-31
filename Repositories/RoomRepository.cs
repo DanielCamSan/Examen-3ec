8c4d766a409a22887973b63611d7f43fea8a8099
majo <maria.sandoval.o@ucb.edu.bo>
2025-10-31 16:37:35 -0400

    feat:Repository

--- Repositories/RoomRepository.cs
+++ Repositories/RoomRepository.cs
@@ -9,9 +9,12 @@ namespace _3ecexamen.Repositories
         private readonly AppDbContext _ctx;
         public RoomRepository(AppDbContext ctx) => _ctx = ctx;
 
-        public Task<bool> ExistsAsync(int id)
+        //TODO: HECHO
+        public async Task<bool> ExistsAsync(int id)
         {
-            //TODO
+            var r = await _ctx.Rooms.FirstOrDefaultAsync(x => x.Id == id);
+            if (r == null) return false;
+            return true;
         }
     }
 }

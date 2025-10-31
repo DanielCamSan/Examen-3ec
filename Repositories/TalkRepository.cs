109a2d03d8fb14a8aa7b1dc0ee266961946de42f
Medrano-Victor <tu-email>
2025-10-31 17:15:30 -0400

    Ft: fixed talkrepository

--- Repositories/TalkRepository.cs
+++ Repositories/TalkRepository.cs
@@ -2,6 +2,9 @@
 using _3ecexamen.Entities;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace _3ecexamen.Repositories
 {
@@ -11,12 +14,19 @@ namespace _3ecexamen.Repositories
         public TalkRepository(AppDbContext ctx) => _ctx = ctx;
 
         public async Task AddAsync(Talk talk) => await _ctx.Talks.AddAsync(talk);
-
-        // Overlap si: (start < existing.End) && (end > existing.Start)
         public Task<bool> HasOverlapAsync(int roomId, DateTime start, DateTime end) =>
             _ctx.Talks.AnyAsync(t => t.RoomId == roomId
                                   && start < t.EndTime
                                   && end > t.StartTime);
+
+        public async Task<List<Talk>> GetBySpeakerAsync(int speakerId)
+        {
+            return await _ctx.Talks
+                .Include(t => t.Speaker)
+                .Include(t => t.Room)
+                .Where(t => t.SpeakerId == speakerId)
+                .ToListAsync();
+        }
 
         public Task<int> SaveChangesAsync() => _ctx.SaveChangesAsync();
     }

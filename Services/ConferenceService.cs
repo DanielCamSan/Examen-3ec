9cae9feea1e761235e489be08a76fd663bd67bf2
AdrianCV2345 <adrianclarosv3@gmail.com>
2025-10-31 17:14:39 -0400

    feat: Services has been created.

--- Services/ConferenceService.cs
+++ Services/ConferenceService.cs
@@ -1,4 +1,5 @@
-﻿using _3ecexamen.DTOs;
+﻿using _3ecexamen.Data;
+using _3ecexamen.DTOs;
 using _3ecexamen.Entities;
 using _3ecexamen.Repositories;
 
@@ -7,10 +8,23 @@ namespace _3ecexamen.Services
     public class ConferenceService : IConferenceService
     {
         //TODO
+        private readonly IConferenceRepository _confs;
+        public ConferenceService(IConferenceRepository conf) => _confs = conf;
 
         public async Task<int> CreateConferenceAsync(CreateConferenceDto dto)
         {
             //TODO
+            var entity = new Conference
+            {
+                Title = dto.Title,
+                StartDate = dto.StartDate,
+                EndDate = dto.EndDate,
+                City = dto.City,
+                Rooms = dto.Rooms.Select(s => new Room { Name = s.Name }).ToList()
+            }; 
+            await _confs.AddAsync(entity);
+            await _confs.SaveChangesAsync();
+            return entity.Id;
         }
 
         public async Task<ConferenceAgendaDto?> GetAgendaAsync(int id)
@@ -18,7 +32,26 @@ namespace _3ecexamen.Services
             var conf = await _confs.GetAgendaAsync(id);
             if (conf == null) return null;
             //TODO  pista: devuelve usando ConferenceAgendaDto
-           
+
+            return new ConferenceAgendaDto
+            {
+                Conference = conf.Title,
+                City = conf.City,
+                Rooms = conf.Rooms.Select(r => new RoomScheduleDto
+                {
+                    Room = r.Name,
+                    Talks = r.Talks.Select(t => new TalkDto
+                    {
+                        SpeakerId = t.SpeakerId,
+                        Speaker = t.Speaker.FullName,
+                        RoomId = t.RoomId,
+                        Room = t.Rooms.Name,
+                        StartTime = t.StartTime,
+                        EndTime = t.EndTime
+                    }).OrderBy(t => t.StartTime).ToList()
+                }).ToList()
+            };
+
         }
     }
 }

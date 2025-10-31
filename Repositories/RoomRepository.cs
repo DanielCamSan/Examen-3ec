961d13f5a36ca2aecbb24a9c5480a6aee8d03882
Josue-Camacho <josuecamacho582@gmail.com>
2025-10-31 17:11:53 -0400

    feat: add ExistsAsync RoomRepository

--- Repositories/RoomRepository.cs
+++ Repositories/RoomRepository.cs
@@ -9,9 +9,12 @@ namespace _3ecexamen.Repositories
         private readonly AppDbContext _ctx;
         public RoomRepository(AppDbContext ctx) => _ctx = ctx;
 
-        public Task<bool> ExistsAsync(int id)
-        {
+        public Task<bool> ExistsAsync(int id)=>
+        
             //TODO
-        }
+            _ctx.Rooms.AnyAsync(s => s.Id == id);
+        
     }
 }
+
+

00b560f61e248f066703ffea9154b6229b202026
Josue-Camacho <josuecamacho582@gmail.com>
2025-10-31 17:17:46 -0400

    feat: add appdbcontext changes

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -19,8 +19,18 @@ namespace _3ecexamen.Data
             //TODO
 
             // 1:N Conference -> Rooms (FK requerida, cascade)
+            modelBuilder.Entity<Room >()
+                .HasOne(c=>c.Conference)
+                .WithMany(r=>r.Rooms)
+                .HasForeignKey(r=>r.ConferenceId)
+                .OnDelete(DeleteBehavior.Cascade);
+
             // N:M con payload: Talk (clave compuesta)
+            modelBuilder.Entity<Talk>()
+                .HasKey(t => new { t.SpeakerId, t.RoomId, t.StartTime });
             // (Opcional) Índice único: Room.Name dentro de una Conference
+            modelBuilder.Entity<Room>()
+                .HasIndex(r => new { r.ConferenceId, r.Name }).IsUnique();
 
         }
     }

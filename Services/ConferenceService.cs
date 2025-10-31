5f70afa056e24a9661305a1f82211fa1c2b8eff0
Alejandro Castro <alejandro.castro.0520@gmail.com>
2025-10-31 17:00:12 -0400

    feat: Some corrections and new logic, implemented GetAgenda

--- Services/ConferenceService.cs
+++ Services/ConferenceService.cs
@@ -8,10 +8,10 @@ namespace _3ecexamen.Services
     public class ConferenceService : IConferenceService
     {
         //TODO
-        private readonly IConferenceRepository _repo;
+        private readonly IConferenceRepository _confs;
         public ConferenceService(IConferenceRepository repo)
         {
-            _repo = repo;
+            _confs = repo;
         }
 
 
@@ -30,8 +30,8 @@ namespace _3ecexamen.Services
                 }).ToList(),
             };
 
-            await _repo.AddAsync(conference);
-            await _repo.SaveChangesAsync();
+            await _confs.AddAsync(conference);
+            await _confs.SaveChangesAsync();
 
             return conference.Id;
         }
@@ -41,6 +41,25 @@ namespace _3ecexamen.Services
             var conf = await _confs.GetAgendaAsync(id);
             if (conf == null) return null;
             //TODO  pista: devuelve usando ConferenceAgendaDto
+
+            ConferenceAgendaDto agenda = new()
+            {
+                Conference = conf.Title,
+                City = conf.City,
+                Rooms = conf.Rooms.Select((roomDto) => new RoomScheduleDto() {
+                    Room=roomDto.Name,
+                    Talks=roomDto.Talks.Select((talkDto)=>new TalkDto()
+                    {
+                        SpeakerId=talkDto.SpeakerId,
+                        Speaker=talkDto.Speaker.FullName,
+                        RoomId=talkDto.RoomId,
+                        Room=talkDto.Room.Name,
+                        StartTime=talkDto.StartTime,
+                        EndTime=talkDto.EndTime,    
+
+                    }).ToList(),
+                }).ToList(),
+            };
            
         }
     }

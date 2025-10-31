7cba09220516c3d19a33bcd2fa7f6c025c5a1b57
unknown <dabnerorozco123@gmail.com>
2025-10-31 17:20:22 -0400

    feat: Creating GetAgendaAsync for Conference Service

--- Controllers/SpeakersController.cs
+++ Controllers/SpeakersController.cs
@@ -1,6 +1,7 @@
-﻿using _3ecexamen.DTOs;
+﻿using Microsoft.AspNetCore.Mvc;
+using _3ecexamen.DTOs;
 using _3ecexamen.Services;
-using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace _3ecexamen.Controllers
 {
@@ -8,27 +9,40 @@ namespace _3ecexamen.Controllers
     [Route("api/v1/[controller]")]
     public class SpeakersController : ControllerBase
     {
-        //TODO  pista: usa speaker y talk service
+        private readonly ISpeakerService _speakerService;
+        private readonly ITalkService _talkService;
+
+        public SpeakersController(ISpeakerService speakerService, ITalkService talkService)
+        {
+            _speakerService = speakerService;
+            _talkService = talkService;
+        }
 
         // POST: api/v1/speakers
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] CreateSpeakerDto dto)
         {
-            //TODO
+            var id = await _speakerService.CreateAsync(dto);
+            return CreatedAtAction(nameof(GetSchedule), new { id }, new { id });
         }
 
         // GET: api/v1/speakers/{id}/schedule
         [HttpGet("{id:int}/schedule")]
         public async Task<IActionResult> GetSchedule(int id)
         {
-            //TODO
+            var schedule = await _speakerService.GetScheduleAsync(id);
+            if (schedule == null)
+                return NotFound();
+
+            return Ok(schedule);
         }
 
         // POST: api/v1/speakers/talks
         [HttpPost("talks")]
         public async Task<IActionResult> AddTalk([FromBody] CreateTalkDto dto)
         {
-            //TODO
+            await _talkService.AddTalkAsync(dto);
+            return Ok();
         }
     }
 }
--- Services/ConferenceService.cs
+++ Services/ConferenceService.cs
@@ -6,19 +6,47 @@ namespace _3ecexamen.Services
 {
     public class ConferenceService : IConferenceService
     {
-        //TODO
+        private readonly IConferenceRepository _conferences;
+
+        public ConferenceService(IConferenceRepository conferences) => _conferences = conferences;
 
         public async Task<int> CreateConferenceAsync(CreateConferenceDto dto)
         {
-            //TODO
+            var entity = new Conference { Title = dto.Title, City = dto.City, StartDate = dto.StartDate, EndDate = dto.EndDate };
+            await _conferences.AddAsync(entity);
+            await _conferences.SaveChangesAsync();
+            return entity.Id;
         }
 
         public async Task<ConferenceAgendaDto?> GetAgendaAsync(int id)
         {
-            var conf = await _confs.GetAgendaAsync(id);
+            var conf = await _conferences.GetAgendaAsync(id);
             if (conf == null) return null;
-            //TODO  pista: devuelve usando ConferenceAgendaDto
-           
+
+            return new ConferenceAgendaDto
+            {
+                Conference = conf.Title,
+                City = conf.City,
+                Rooms = conf.Rooms
+                    .Select(r => new RoomScheduleDto
+                    {
+                        Room = r.Name,
+                        Talks = r.Talks
+                            .OrderBy(t => t.StartTime)
+                            .Select(t => new TalkDto
+                            {
+                                SpeakerId = t.Speaker.Id,
+                                Speaker = t.Speaker.FullName,
+                                RoomId = r.Id,
+                                Room = r.Name,
+                                StartTime = t.StartTime,
+                                EndTime = t.EndTime
+                            })
+                            .ToList()
+                    })
+                    .ToList()
+            };
         }
+
     }
 }
